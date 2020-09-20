72eb4371e2c55e703a6ead686151f84558d74562
Eric Eichhorn <ee.eric.eichhorn@gmail.com>
2020-09-20 18:42:10 +0200

    cors

--- WaveBall.Api2/Startup.cs
+++ WaveBall.Api2/Startup.cs
@@ -24,6 +24,14 @@ namespace WaveBall.Api2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:8080");
+                    });
+            });
             services.AddControllers();
         }
 
@@ -36,6 +44,8 @@ namespace WaveBall.Api2
             }
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
