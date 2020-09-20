d0cd10b9e7458e2d2edbc737687582958885b446
Eric Eichhorn <ee.eric.eichhorn@gmail.com>
2020-09-20 19:28:46 +0200

    health

---
+++ WaveBall.Api2/Controllers/HealthController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WaveBall.Api2.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "I'm fine :)";
+        }
+    }
+}
